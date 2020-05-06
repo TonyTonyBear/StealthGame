4c46ab1aedc72c0d2710667ae0dee717ab6b823d
Tony Barrett <ajbarrettx@gmail.com>
2020-05-06 00:59:10 -0400

    Update logic in source files to reflect blueprint prototype camera movement.

--- Source/FPSGame/Private/FPSGameMode.cpp
+++ Source/FPSGame/Private/FPSGameMode.cpp
@@ -3,7 +3,10 @@
 #include "FPSGameMode.h"
 #include "FPSHUD.h"
 #include "FPSCharacter.h"
+#include "Kismet/GameplayStatics.h"
 #include "UObject/ConstructorHelpers.h"
+
+#define OUT
 
 AFPSGameMode::AFPSGameMode()
 {
@@ -22,6 +25,23 @@ void AFPSGameMode::CompleteMission(APawn* InstigatorPawn)
 	if (InstigatorPawn)
 	{
 		InstigatorPawn->DisableInput(nullptr); // We use nullptr to get the default character controller for the game mode.
+
+		if (SpectatingViewpointClass)
+		{
+			APlayerController* Controller = Cast<APlayerController>(InstigatorPawn);
+
+			TArray<AActor*> Spectators;
+			UGameplayStatics::GetAllActorsOfClass(this, SpectatingViewpointClass, OUT Spectators);
+
+			if (Controller && Spectators.Num() > 0)
+			{
+				Controller->SetViewTargetWithBlend(Spectators[0], 0.5f, EViewTargetBlendFunction::VTBlend_Cubic);
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("SpectatingViewpointClass is nullptr. Please assign a value to this field."));
+		}
 	}
 
 	OnMissionCompleted(InstigatorPawn);
